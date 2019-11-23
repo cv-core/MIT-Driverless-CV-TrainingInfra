7375b81beeeeb9365e6b5025caa69bea3842e871
Sibo Zhu <siboz1995@gmail.com>
2019-11-23 13:16:36 -0500

    update

---
+++ utilities/install_content/training.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+pip2 install onnx==1.1.1 google_compute_engine Pillow pycuda==2018.1.1 numpy opencv-python
+pip3 install opencv-python imgaug tqdm gnupg setuptools six typing pyyaml future pandas torch torchvision google-cloud-storage matplotlib pycuda
+
+# Fix a wierd cuda nvcc issue, potentially not needed with future builds
+sudo apt-get install gcc-5 g++-5 -y
+cd /usr/lib/nvidia-cuda-toolkit/bin
+sudo rm gcc g++
+sudo ln -s /usr/bin/gcc-5 gcc
+sudo ln -s /usr/bin/g++-5 g++
+
+cd ~
+git clone git@github.com:cv-core/PerceptionCV.git 
+cd ~/PerceptionCV/CVC-YOLOv3/
+sudo python3 setup.py build develop
+echo 'PYTHONPATH=$PYTHONPATH:/home/$USER/PerceptionCV/CVC-YOLOv3/utils/' >> ~/.bashrc
+echo 'cd ~/PerceptionCV/CVC-YOLOv3' >> ~/.bashrc
